53f5a47870eba6a1e7828a92afa7ffdf67ad6875
Akash Indulkar <akashindulkar6677@gmail.com>
2025-05-13 11:54:02 -0700

    Understanding maven and hibernate

---
+++ HIBERNATE-EXAMPLE/src/main/java/com/akash/java/HIBERNATE_EXAMPLE/App.java
@@ -0,0 +1,9 @@
+package com.akash.java.HIBERNATE_EXAMPLE;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ HIBERNATE-EXAMPLE/src/main/java/com/akash/java/HIBERNATE_EXAMPLE/Car.java
@@ -0,0 +1,61 @@
+package com.akash.java.HIBERNATE_EXAMPLE;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity //to let know hibernate that mapping is provided in this class
+@Table(name="car") //to map a class to a table in db
+public class Car {
+	@Id() //to mark a data member as primary key and it is mandatory provide primary key
+	@GeneratedValue(strategy = GenerationType.AUTO) //to auto increment the primary key
+	@Column(name="carid") //to map a data member to a column in table
+	int carId;
+	@Column(name="modelname")
+	String modelName;
+	@Column(name="price")
+	float price;
+	@Column(name="color")
+	String color;
+	
+	public Car(int carId, String modelName, float price, String color) {
+		super();
+		this.carId = carId;
+		this.modelName = modelName;
+		this.price = price;
+		this.color = color;
+	}
+	
+	@Override
+	public String toString() {
+		return "Car [carId=" + carId + ", modelName=" + modelName + ", price=" + price + ", color=" + color + "]";
+	}
+
+	public int getCarId() {
+		return carId;
+	}
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+	public String getModelName() {
+		return modelName;
+	}
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
